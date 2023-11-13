d9a53bc42a888cc1e9b8fb060ca2810f335ca5cb
jesn1219 <jesn1219@gmail.com>
2023-11-13 06:26:54 +0000

    run_cont.sh script modified

--- run_cont.sh
+++ run_cont.sh
@@ -4,41 +4,51 @@
 echo "Creating tmux session 'gpumon'..."
 tmux new-session -d -s gpumon
 
+# Define a minimum required free GPU memory (in MB) to run 'run.sh'
+min_required_gpu_ram=1000  # Adjust this value as needed
+
+# Initialize occupied_ram variable
+occupied_ram=0
+
 while true; do
-    # Check the total and available GPU memory
-    echo "Checking GPU memory..."
-    gpu_ram=$(nvidia-smi --query-gpu=memory.total --format=csv,noheader,nounits)
+    # Check the available GPU memory
+    echo "Checking available GPU memory..."
     gpu_ram_available=$(nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits)
-    echo "Total GPU RAM: $gpu_ram MB, Available GPU RAM: $gpu_ram_available MB"
 
-    # Generate a unique identifier for each run
-    unique_id=$(date +%s)
+    echo "Available GPU RAM: $gpu_ram_available MB"
 
-    # Create a new window for running 'run.sh' and execute it
-    echo "Running 'run.sh'..."
-    window_name="run_script_$unique_id"
-    tmux new-window -t gpumon -n $window_name
-    tmux send-keys -t gpumon:$window_name './run.sh' C-m
-    sleep 15
+    # Calculate total memory requirement considering the occupied memory
+    total_memory_required=$((min_required_gpu_ram + occupied_ram))
 
-    # Check GPU memory usage after running 'run.sh'
-    echo "Checking GPU memory usage after running 'run.sh'..."
-    gpu_ram_after=$(nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits)
-    run_gpu_occupied=$((gpu_ram_available - gpu_ram_after))
-    echo "GPU RAM used by 'run.sh': $run_gpu_occupied MB"
+    # Check if available GPU memory is greater than the total required memory
+    if (( gpu_ram_available > total_memory_required )); then
+        echo "Sufficient memory available, running 'run.sh'..."
 
-    # Compare the current available GPU RAM with the usage after running 'run.sh'
-    if (( gpu_ram_after > run_gpu_occupied + 600 )); then
-        echo "Sufficient memory available, running 'run.sh' again..."
+        # Save the current available memory before running 'run.sh'
+        gpu_ram_before_run=$gpu_ram_available
+
+        # Generate a unique identifier for this run
         unique_id=$(date +%s)
-        window_name="run_script_$unique_id"
-        tmux new-window -t gpumon -n $window_name
-        tmux send-keys -t gpumon:$window_name './run.sh' C-m
-    else
-        echo "Not enough memory to run 'run.sh' again."
-    fi
 
-    # Wait for a while before checking again
-    echo "Waiting for the next check..."
-    sleep 10
+        # Create a new window in tmux for running 'run.sh' and execute it
+        window_name="run_script_$unique_id"
+        tmux new-window -t gpumon -n "$window_name"
+        tmux send-keys -t gpumon:"$window_name" './run.sh' C-m
+
+        # Wait for some time to let 'run.sh' start and occupy memory
+        sleep 25
+
+        # Check the available GPU memory after running 'run.sh'
+        gpu_ram_after_run=$(nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits)
+
+        # Calculate the memory occupied by 'run.sh'
+        run_gpu_occupied=$((gpu_ram_before_run - gpu_ram_after_run))
+
+        # Update the occupied_ram for the next iteration
+        occupied_ram=$((occupied_ram + run_gpu_occupied))
+
+        echo "Memory occupied by 'run.sh': $run_gpu_occupied MB"
+    else
+        echo "Not enough memory to run 'run.sh'."
+    fi
 done
