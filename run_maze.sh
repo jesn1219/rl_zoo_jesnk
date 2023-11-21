8b9dd1c14fb0c3c8df8cc24281b5ada5fa6b5ec2
jesn1219 <jesn1219@gmail.com>
2023-11-21 04:24:08 +0000

    PointMaze hyperparams initialized

--- run_maze.sh
+++ run_maze.sh
@@ -1,1 +1,10 @@
-python ./train.py --env PointMaze_Large-v3 --algo tqc  --wandb-project-name baselines --track --save-freq 100000 --save-replay-buffer -tags maze 
+python ./train.py \
+--env PointMaze_Large-v3 \
+--algo tqc \
+--wandb-project-name baselines \
+--track \
+--save-freq 1000 \
+--save-replay-buffer \
+--env-kwargs continuing_task:False \
+-tags maze,no_continuing_task \
+-params n_timesteps:1000000
