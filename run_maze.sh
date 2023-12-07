66e2f5ef1fa3b137fae52624b36618c2b3d86fa4
jesn1219 <jesn1219@gmail.com>
2023-12-07 13:03:21 +0000

    attention_v1

--- run_maze.sh
+++ run_maze.sh
@@ -1,10 +1,10 @@
 python ./train.py \
---env PointMaze_Large-v3 \
+--env PointMaze_UMaze-v3 \
 --algo tqc \
 --wandb-project-name baselines \
 --track \
 --save-freq 1000 \
 --save-replay-buffer \
 --env-kwargs continuing_task:False \
--tags maze,no_continuing_task,g095 \
+-tags maze,no_continuing_task,g095,attention_v1 \
 -params n_timesteps:1000000 gamma:0.95
